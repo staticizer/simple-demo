4cc3d1d0ca9c9afe03c163b2bafc0b5b4a283af1
BobNobrain <nikita-ivanov.mail@yandex.ru>
2019-06-03 21:04:54 +0300

    Switching to use of staticizer as a separate package

--- loaders/site-loader.js
+++
@@ -1,6 +0,0 @@
-// const requireFromString = require('require-from-string');
-
-module.exports = function (src) {
-    console.log(src);
-    return 'module.exports = 0;';
-}
--- staticizer.config.js
+++ staticizer.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    output: {
+        assetsPrefix: 'assets',
+        staticPrefix: false,
+        minify: {
+            html: false,
+            js: false,
+            css: false
+        }
+    },
+
+    layout: {
+        default: 'default'
+    },
+
     data: {
         common: {
             meta: {
